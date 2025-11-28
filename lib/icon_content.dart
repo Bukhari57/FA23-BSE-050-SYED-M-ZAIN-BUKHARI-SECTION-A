3e6c263fc8008801bce9d930487a7c444b987859
syed zain bukhari <shahsyed1120@gmail.com>
2025-11-28 11:12:09 +0500

    PART 10( CALCULATOR SLIDER)
    IN THIS STEPP I HAVE ADDED CALCULATOR SLIDER IN BMI APP

---
+++ lib/icon_content.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  const IconContent({super.key, required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FaIcon(
+          icon,
+          size: 80.0,
+          color: const Color(0xFF8D8E98),
+        ),
+        const SizedBox(height: 15.0),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        ),
+      ],
+    );
+  }
+}
