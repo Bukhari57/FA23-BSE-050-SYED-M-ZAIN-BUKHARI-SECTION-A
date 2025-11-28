484d29ca119ebf985ff2fe1ca250e34df8395d36
syed zain bukhari <shahsyed1120@gmail.com>
2025-11-28 10:25:15 +0500

    PART 6 (USE OF ENUM)
    IN THIS VIDEO I HAVE USED THE ENUM IN THIS BMI APP

--- bmi/lib/main.dart
+++ bmi/lib/main.dart
@@ -3,26 +3,28 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'reusable_card.dart';
 import 'icon_content.dart';
 
-void main() {
-  runApp(const BMICalculator());
-}
+const kActiveColor = Color(0xFF1D1E33);
+const kInactiveColor = Color(0xFF111328);
+const kBottomContainerColor = Color(0xFFEB1555);
+
+enum Gender { male, female }
+
+void main() => runApp(const BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   const BMICalculator({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: const Color(0xFF0A0E21),
-        scaffoldBackgroundColor: const Color(0xFF0A0E21),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Color(0xFF0A0E21),
-        ),
+  Widget build(BuildContext context) => MaterialApp(
+    theme: ThemeData.dark().copyWith(
+      primaryColor: const Color(0xFF0A0E21),
+      scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color(0xFF0A0E21),
       ),
-      home: const InputPage(),
-    );
-  }
+    ),
+    home: const InputPage(),
+  );
 }
 
 class InputPage extends StatefulWidget {
@@ -33,50 +35,52 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender? selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
-        centerTitle: true,
-      ),
+      appBar: AppBar(title: const Text('BMI CALCULATOR')),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Row(
               children: [
                 Expanded(
                   child: ReusableCard(
-                    colour: const Color(0xFF1D1E33),
-                    cardChild: const IconContent(
-                      icon: FontAwesomeIcons.mars,
-                      label: 'MALE',
-                    ),
+                    onPress: () => setState(() => selectedGender = Gender.male),
+                    colour: selectedGender == Gender.male ? kActiveColor : kInactiveColor,
+                    cardChild: const IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: const Color(0xFF1D1E33),
-                    cardChild: const IconContent(
-                      icon: FontAwesomeIcons.venus,
-                      label: 'FEMALE',
-                    ),
+                    onPress: () => setState(() => selectedGender = Gender.female),
+                    colour: selectedGender == Gender.female ? kActiveColor : kInactiveColor,
+                    cardChild: const IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE'),
                   ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReusableCard(colour: const Color(0xFF1D1E33)),
+            child: ReusableCard(
+              colour: kActiveColor,
+            ),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReusableCard(colour: const Color(0xFF1D1E33)),
+                  child: ReusableCard(
+                    colour: kActiveColor,
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(colour: const Color(0xFF1D1E33)),
+                  child: ReusableCard(
+                    colour: kActiveColor,
+                  ),
                 ),
               ],
             ),
