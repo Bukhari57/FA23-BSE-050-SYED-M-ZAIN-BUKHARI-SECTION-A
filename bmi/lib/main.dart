7705758cfe405b015d00cd04a6fbeb65008eef35
syed zain bukhari <shahsyed1120@gmail.com>
2025-11-28 09:44:07 +0500

    PART 3
    IN THIS VIDEO I HAVE CHANGED THE CONTAINER INTO A CLASS

--- bmi/lib/main.dart
+++ bmi/lib/main.dart
@@ -37,12 +37,56 @@ class _InputPageState extends State<InputPage> {
         title: const Text('BMI CALCULATOR'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'Body goes here',
-          style: TextStyle(color: Colors.white),
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(colour: const Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: const Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(colour: const Color(0xFF1D1E33)),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(colour: const Color(0xFF1D1E33)),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: const Color(0xFF1D1E33)),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+  final Widget? cardChild;
+
+  const ReusableCard({super.key, required this.colour, this.cardChild});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: cardChild,
     );
   }
 }
